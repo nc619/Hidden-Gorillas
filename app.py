8cfaf9bb3f9155bd351d284aac7feaf925801673
James Dorricott <jamie.dorricott@hotmail.com>
2023-09-24 11:28:33 +0100

    Updated app script

--- app.py
+++ app.py
@@ -56,57 +56,80 @@ def main():
 
     # --------- Animated Text --------- #
 
-    # Check if 'counter' exists in the session state
-    if 'counter' not in st.session_state:
-        st.session_state.counter = 0
+    # Check if 'scenario_complete' exists in the session state. If not, set to False
+    if 'scenario_complete' not in st.session_state:
+        st.session_state.scenario_complete = False
 
-    # Generate or fetch your clinical scenario.
-    clinical_scenario_parts = [clinical_example]
+    # Only animate the clinical scenario if it hasn't been completed
+    if not st.session_state.scenario_complete:
 
-    # Create a message container for the clinical scenario
-    with st.chat_message("Clinical Scenario"):
-        message_placeholder = st.empty()
-        full_response = ""
+        # Check if 'counter' exists in the session state
+        if 'counter' not in st.session_state:
+            st.session_state.counter = 0
 
-        # Display clinical scenario up to the current counter with typing effect
-        for part in clinical_scenario_parts[:st.session_state.counter + 1]:
-            for chunk in part.split():
-                full_response += chunk + " "
-                time.sleep(0.075)  # Adjust the delay as required
-                # Add a blinking cursor to simulate typing
-                message_placeholder.markdown(full_response + "▌")
-            full_response += '\n'  # Add newline after each part
-        message_placeholder.markdown(full_response)
+        # Generate or fetch your clinical scenario.
+        clinical_scenario_parts = [clinical_example] 
 
-    # ========= Generate Image ========= #
+        # Create a message container for the clinical scenario
+        with st.chat_message("Clinical Scenario"):
+            message_placeholder = st.empty()
+            full_response = ""
 
-    # # Display the generated clinical image
-    # image_url = generate_image()
-    # time.sleep(0.075)  # Adjust the delay as required
-    # st.image(image_url, caption='Generated Clinical Image', use_column_width=True)
+            # Display clinical scenario up to the current counter with typing effect
+            for part in clinical_scenario_parts[:st.session_state.counter + 1]:
+                for chunk in part.split():
+                    full_response += chunk + " "
+                    time.sleep(0.075)  # Adjust the delay as required
+                    # Add a blinking cursor to simulate typing
+                    message_placeholder.markdown(full_response + "▌")
+                full_response += '\n'  # Add newline after each part
+            message_placeholder.markdown(full_response)
+    
+    else:
+    # If scenario is already displayed, just display it without animation
+        with st.chat_message("Clinical Scenario"):
+            st.write(clinical_example)
+
+    # # ========= Generate Image ========= #
 
     # If 'show_image' doesn't exist in session_state, initialize with False
     if 'show_image' not in st.session_state:
         st.session_state.show_image = False
 
     # Button to show image
-    if st.button('Continue to Image'):
-        st.session_state.show_image = True
+    if not st.session_state.show_image:
+        if st.button('Continue to Image'):
+            st.session_state.show_image = True
 
     # Display the image after "Continue to Image" is pressed
     if st.session_state.show_image:
-        st.markdown("<style>@keyframes fadeIn {from {opacity: 0;} to {opacity: 1;}}</style>", unsafe_allow_html=True)
-        st.image(generate_image(prompt), caption='Generated Clinical Image', use_column_width=True)
 
-        # ========= Student Answer ========= #
+        # Display the generated clinical image
+        image_url = generate_image()
+        time.sleep(0.075)  # Adjust the delay as required
+        st.image(image_url, caption='Generated Clinical Image', use_column_width=True)
 
-        # Student DISCUSSION HERE
+        # # ========= Student Answer ========= #
 
-        # Display "Submit" and "New Scenario" buttons
-        if st.button('Submit'):
-            st.write("Submitting...")  # Placeholder, replace with actual submission logic
+        # Insert Free Text Box, where users type in something...
+        # and then Going to insert a conversational chatgpt function call here 
+        # which will evaluate the response
+        user_input = st.text_input("Your analysis:")
 
-        if st.button('New Scenario'):
+        # If the user has input something, simulate a call to ChatGPT (or your own function)
+        if user_input:
+            # This is a mock function to simulate a response. Replace with your actual logic or API call.
+            def chat_gpt_response(user_text):
+                # Dummy logic; replace with real function call
+                if "ailment" in user_text.lower():
+                    return "It seems you've identified the ailment. Good job!"
+                else:
+                    return "Not quite right. Try again."
+
+            response = chat_gpt_response(user_input)
+            st.write(response)
+
+        if st.button('New Scenario', key='newScenarioBtn'):
             # Reset states for a new scenario
             st.session_state.counter = 0
             st.session_state.show_image = False
